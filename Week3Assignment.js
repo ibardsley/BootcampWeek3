35f6ee5df55c43b3a7c0906e8b186148c6b8070e
Caasi621 <110862152+Caasi621@users.noreply.github.com>
2022-10-17 20:40:22 -0400

    Updates to coding

---
+++ Week3Assignment.js
@@ -0,0 +1,16 @@
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(ages);
+
+var subtract = ages[ages.length - 1] - ages[0];
+console.log(subtract);
+
+ages.push(37);
+console.log(ages);
+
+var subtract = ages[ages.length - 1] - ages[0];
+console.log(subtract);
+
+for(i = 0; i < ages.length; i++){
+    
+}
+    
